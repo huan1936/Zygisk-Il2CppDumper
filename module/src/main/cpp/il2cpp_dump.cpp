516ba9a6482bb0bb2b0e2be880e5c36bb094c1b5
huan1936 <1936297424@qq.com>
2026-01-22 23:40:03 +0800

    尝试修复

--- module/src/main/cpp/il2cpp_dump.cpp
+++ module/src/main/cpp/il2cpp_dump.cpp
@@ -322,9 +322,49 @@ std::string dump_type(const Il2CppType *type) {
     return outPut.str();
 }
 
+uintptr_t find_library(const char *library) {
+    char filename[0xFF] = {0},
+            buffer[1024] = {0};
+    FILE *fp = NULL;
+    uintptr_t address = 0, start = 0, end = 0;
+    char flags[7], path[PATH_MAX];
+    pid_t pid = getpid();
+    sprintf(filename, "/proc/%d/maps", pid);
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        perror("fopen");
+        goto done;
+    }
+    while (fgets(buffer, sizeof(buffer), fp)) {
+        strcpy(path, "");
+        sscanf(buffer, "%" PRIxPTR "-%" PRIxPTR " %s %*x %*x:%*x %*u %s\n", &start, &end, flags, path);
+        if (strstr(flags, "r-xp") == 0) continue;
+        if (strstr(path, library)) {
+            address = start;
+            goto done;
+        }
+    }
+    done:
+    if (fp) {
+        fclose(fp);
+    }
+    return address;
+}
+
+#define OFFSET_il2cpp_domain_get_assemblies 0x19F2C4
+
+
 void il2cpp_api_init(void *handle) {
     LOGI("il2cpp_handle: %p", handle);
     init_il2cpp_api(handle);
+
+    if (!il2cpp_domain_get_assemblies) {
+        uintptr_t base = find_library("libil2cpp.so");
+        il2cpp_domain_get_assemblies = (const Il2CppAssembly** (*)(const Il2CppDomain*, size_t*))
+                (base + OFFSET_il2cpp_domain_get_assemblies);
+
+    }
+
     if (il2cpp_domain_get_assemblies) {
         Dl_info dlInfo;
         if (dladdr((void *) il2cpp_domain_get_assemblies, &dlInfo)) {
